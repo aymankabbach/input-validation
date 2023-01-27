935254c405879f63848a7e6cc5d75681be81ff6a
ayman <kabbachayman95@gmail.com>
2023-01-27 18:43:32 +0100

    create function checks number between 2 numbers

--- clsInputValidate.h
+++ clsInputValidate.h
@@ -4,5 +4,16 @@ using namespace std;
 
 class clsInputValidate
 {
-
+public:
+    static bool is_number_between(short number, short from, short to)
+    {
+        if (from<number && number<=to)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 };
--- input_and_validation_library.cpp
+++ input_and_validation_library.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "clsInputValidate.h"
 using namespace std;
 
 int main()
 { 
+    cout<<clsInputValidate::is_number_between(12,0,20)<<endl;
     system("pause");
     return 0;
 }
